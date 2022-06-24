2c7ec00b66ae929dd4128e78c55c557228b0bdc3
icebryant <1103633343@qq.com>
2022-06-25 00:22:33 +0800

    add modle

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cool = require('cool-ascii-faces')
 const path = require('path')
+const mongoose = require('mongoose')
 // var morgan = require('morgan')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 app.use(cors())
@@ -22,6 +25,8 @@ const requestLogger = (request, response, next) => {
   next()
 }
 app.use(requestLogger)
+
+
 // const unknownEndpoint = (request, response) => {
 //   response.status(404).send({ error: 'unknown endpoint' })
 // }
@@ -29,66 +34,82 @@ app.use(requestLogger)
 // app.use(unknownEndpoint)
 
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
 // app.get('/', (req, res) => {
 //   res.send('<h1>Hello World!</h1>')
 // })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes)
+  })
 })
-app.get('/api/notes/:id', (req, res) => {
-  const id = parseFloat(req.params.id)
-  console.log(id)
-  const note = notes.find((note) => note.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    res.send('404 not found')
-    res.status(404).end()
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-})
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter((note) => note.id !== id)
-  res.json(notes)
-  res.status(204).end()
-})
 
-app.post('/api/notes', (request, response) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  next(error)
+}
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
-  const note = request.body
-  note.id = maxId + 1
 
-  notes = notes.concat(note)
-  response.json(notes)
-})
-
-// const PORT = 3001
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
